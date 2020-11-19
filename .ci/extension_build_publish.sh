5c0d1abcd9045615d7607deace33921e63509505
Eric Williams <ericwill@redhat.com>
2020-11-19 09:54:19 -0500

    Clean up some things: (#5)
    * Dockerfile
    * Add meaningful tag names to the GitHub action
    * Build/publish script
    
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- .ci/extension_build_publish.sh
+++ .ci/extension_build_publish.sh
@@ -20,22 +20,21 @@ do
     if [[ $file == */extension.json ]]; then
         EXTENSION_NAME=$(echo "$file" | cut -d/ -f 1)
         if ! [[ " ${EXTENSIONS_TO_BUILD[@]} " =~ ${EXTENSION_NAME} ]]; then
-            EXTENSIONS_TO_BUILD+=("$SIDECAR_NAME")
-            EXTENSION_REPOSITORY=$(cat $file | jq -r .repository)
-            EXTENSION_REVISION=$(cat $file | jq -r .revision)
+            EXTENSIONS_TO_BUILD+=("$EXTENSION_NAME")
+            EXTENSION_REPOSITORY=$(cat "$file" | jq -r .repository)
+            EXTENSION_REVISION=$(cat "$file" | jq -r .revision)
             echo "Building $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
             if test -f "$EXTENSION_NAME/Dockerfile"; then
-                docker build --no-cache=true --build-arg extension_name=$EXTENSION_NAME --build-arg extension_repository=$EXTENSION_REPOSITORY \
-                    --build-arg extension_revision=$EXTENSION_REVISION -t $EXTENSION_NAME-builder $EXTENSION_NAME/
-                # echo "Nested Dockerfile"
+                docker build --no-cache=true --build-arg extension_name="$EXTENSION_NAME" --build-arg extension_repository="$EXTENSION_REPOSITORY" \
+                    --build-arg extension_revision="$EXTENSION_REVISION" -t "$EXTENSION_NAME"-builder "$EXTENSION_NAME"/
             else
-                docker build --no-cache=true --build-arg extension_name=$EXTENSION_NAME --build-arg extension_repository=$EXTENSION_REPOSITORY \
-                    --build-arg extension_revision=$EXTENSION_REVISION -t $EXTENSION_NAME-builder .
+                docker build --no-cache=true --build-arg extension_name="$EXTENSION_NAME" --build-arg extension_repository="$EXTENSION_REPOSITORY" \
+                    --build-arg extension_revision="$EXTENSION_REVISION" -t "$EXTENSION_NAME"-builder .
             fi
             if [[ $BUILD_PUBLISH == 'build-publish' ]]; then
                 echo "Publishing $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
-                docker run --cidfile $EXTENSION_NAME-builder-id $EXTENSION_NAME-builder
-                BUILDER_CONTAINER_ID=$(cat $EXTENSION_NAME-builder-id)
+                docker run --cidfile "$EXTENSION_NAME"-builder-id "$EXTENSION_NAME"-builder
+                BUILDER_CONTAINER_ID=$(cat "$EXTENSION_NAME"-builder-id)
                 docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION.vsix ./
                 docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION-sources.tar.gz ./
                 docker stop $BUILDER_CONTAINER_ID
