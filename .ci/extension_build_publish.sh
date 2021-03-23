53f8db2a74d91028498aafe0cb454aea742c3893
Eric Williams <ericwill@redhat.com>
2021-03-23 14:28:07 -0400

    Build CI Improvements (#20)
    * Trigger the workflow on changes to Dockerfiles, not just
      extension.json files
    * Modify build script to pick up changes from nested Dockerfiles
    
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- .ci/extension_build_publish.sh
+++ .ci/extension_build_publish.sh
@@ -19,27 +19,35 @@ for file in "${FILES_CHANGED[@]}"
 do
     if [[ $file == */extension.json ]]; then
         EXTENSION_NAME=$(echo "$file" | cut -d/ -f 1)
-        if ! [[ " ${EXTENSIONS_TO_BUILD[@]} " =~ ${EXTENSION_NAME} ]]; then
-            EXTENSIONS_TO_BUILD+=("$EXTENSION_NAME")
-            EXTENSION_REPOSITORY=$(cat "$file" | jq -r .repository)
-            EXTENSION_REVISION=$(cat "$file" | jq -r .revision)
-            echo "Building $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
-            if test -f "$EXTENSION_NAME/Dockerfile"; then
-                docker build --no-cache=true --build-arg extension_name="$EXTENSION_NAME" --build-arg extension_repository="$EXTENSION_REPOSITORY" \
-                    --build-arg extension_revision="$EXTENSION_REVISION" -t "$EXTENSION_NAME"-builder "$EXTENSION_NAME"/
-            else
-                docker build --no-cache=true --build-arg extension_name="$EXTENSION_NAME" --build-arg extension_repository="$EXTENSION_REPOSITORY" \
-                    --build-arg extension_revision="$EXTENSION_REVISION" -t "$EXTENSION_NAME"-builder .
-            fi
-            if [[ $BUILD_PUBLISH == 'build-publish' ]]; then
-                echo "Publishing $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
-                docker run --cidfile "$EXTENSION_NAME"-builder-id "$EXTENSION_NAME"-builder
-                BUILDER_CONTAINER_ID=$(cat "$EXTENSION_NAME"-builder-id)
-                docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION.vsix ./
-                docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION-sources.tar.gz ./
-                docker stop $BUILDER_CONTAINER_ID
-                rm ./$EXTENSION_NAME-builder-id
-            fi
+        EXTENSION_JSON=$file
+    elif [[ $file == */Dockerfile ]]; then
+        EXTENSION_NAME=$(echo "$file" | cut -d/ -f 1)
+        if test -f "$EXTENSION_NAME/extension.json"; then
+            EXTENSION_JSON="$EXTENSION_NAME/extension.json"
+        else
+            continue
+        fi
+    fi
+    if ! [[ " ${EXTENSIONS_TO_BUILD[@]} " =~ ${EXTENSION_NAME} ]]; then
+        EXTENSIONS_TO_BUILD+=("$EXTENSION_NAME")
+        EXTENSION_REPOSITORY=$(cat "$EXTENSION_JSON" | jq -r .repository)
+        EXTENSION_REVISION=$(cat "$EXTENSION_JSON" | jq -r .revision)
+        echo "Building $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
+        if test -f "$EXTENSION_NAME/Dockerfile"; then
+            docker build --no-cache=true --build-arg extension_name="$EXTENSION_NAME" --build-arg extension_repository="$EXTENSION_REPOSITORY" \
+                --build-arg extension_revision="$EXTENSION_REVISION" -t "$EXTENSION_NAME"-builder "$EXTENSION_NAME"/
+        else
+            docker build --no-cache=true --build-arg extension_name="$EXTENSION_NAME" --build-arg extension_repository="$EXTENSION_REPOSITORY" \
+                --build-arg extension_revision="$EXTENSION_REVISION" -t "$EXTENSION_NAME"-builder .
+        fi
+        if [[ $BUILD_PUBLISH == 'build-publish' ]]; then
+            echo "Publishing $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
+            docker run --cidfile "$EXTENSION_NAME"-builder-id "$EXTENSION_NAME"-builder
+            BUILDER_CONTAINER_ID=$(cat "$EXTENSION_NAME"-builder-id)
+            docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION.vsix ./
+            docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION-sources.tar.gz ./
+            docker stop $BUILDER_CONTAINER_ID
+            rm ./$EXTENSION_NAME-builder-id
         fi
     fi
 done
