3fc8388a45859c7b6108b2845349c4e3a0a980aa
Samantha Dawley <sdawley@redhat.com>
2023-04-12 12:47:18 -0700

    CRW-4049 (#56)
    * CRW-4049 Adding all plugins for devspaces-pluginregistry
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * CRW-4049 Add parameters for dependency manager, vsce version and ubi image.
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * Adding build scripts
    
    * Adding plugin-config.json, updateing build.sh. Fix for ms-python.python
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * Update jupyter Dockerfile so I can share it with the world...
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * Remove extension jsons. Add javascript plugins.
    
    * Fix update.sh
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * Try adding libsecret for keytar
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * Removing libsecret I know what's happening now
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    * Updating vscode-js-profile-table dockerfile and setting most plugins to do-not-update for faster testing.
    
    Signed-off-by: sdawley <sdawley@redhat.com>
    
    ---------
    
    Signed-off-by: sdawley <sdawley@redhat.com>

---
+++ build/build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+set -ex
+
+EXTENSION_NAME="$1"
+
+if [[ $(cat "plugin-config.json" | jq -r '.Plugins["'$EXTENSION_NAME'"]') -eq "null" ]]; then
+    echo "Extension $EXTENSION_NAME is not in plugin-config.json"
+    exit
+fi
+
+function parse_json () {
+    echo $(cat "plugin-config.json" | jq -r '.Plugins["'$EXTENSION_NAME'"]["'$1'"]')
+}
+
+EXTENSION_REPOSITORY=$(parse_json repository)
+EXTENSION_REVISION=$(parse_json revision)
+
+#Defaults
+ubi8Image="nodejs-18:1-24"
+packageManager="npm@latest"
+vsceVersion="2.17.0"
+
+EXTENSION_IMAGE=$(parse_json ubi8Image)
+if [[ $EXTENSION_IMAGE -eq "null" ]]; then
+    EXTENSION_IMAGE=$ubi8Image
+fi
+
+EXTENSION_MANAGER=$(parse_json packageManager)
+if [[ $EXTENSION_MANAGER -eq "null" ]]; then
+    EXTENSION_MANAGER=$packageManager
+fi
+
+EXTENSION_VSCE=$(parse_json vsceVersion)
+if [[ $EXTENSION_VSCE -eq "null" ]]; then
+    EXTENSION_VSCE=$vsceVersion
+fi
+
+echo "Building $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
+if test -f "$EXTENSION_NAME/Dockerfile"; then
+    docker build --no-cache=true \
+        --build-arg extension_name="$EXTENSION_NAME" \
+        --build-arg extension_repository="$EXTENSION_REPOSITORY" \
+        --build-arg extension_revision="$EXTENSION_REVISION" \
+        --build-arg extension_image="$EXTENSION_IMAGE" \
+        --build-arg extension_manager="$EXTENSION_MANAGER" \
+        --build-arg extension_vsce="$EXTENSION_VSCE" \
+        -t "$EXTENSION_NAME"-builder "$EXTENSION_NAME"/
+else
+    docker build --no-cache=true \
+        --build-arg extension_name="$EXTENSION_NAME" \
+        --build-arg extension_repository="$EXTENSION_REPOSITORY" \
+        --build-arg extension_revision="$EXTENSION_REVISION" \
+        --build-arg extension_image="$EXTENSION_IMAGE" \
+        --build-arg extension_manager="$EXTENSION_MANAGER" \
+        --build-arg extension_vsce="$EXTENSION_VSCE" \
+        -t "$EXTENSION_NAME"-builder .
+fi
+
+echo "Publishing $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
+docker run --cidfile "$EXTENSION_NAME"-builder-id "$EXTENSION_NAME"-builder
+BUILDER_CONTAINER_ID=$(cat "$EXTENSION_NAME"-builder-id)
+docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME.vsix ./
+#docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-$EXTENSION_REVISION-sources.tar.gz ./
+docker stop $BUILDER_CONTAINER_ID
+rm ./$EXTENSION_NAME-builder-id
---
+++ build/update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+set -e
+
+PLUGINS=$(cat plugin-config.json | jq -r '.Plugins | keys[]')
+UPDATES=""
+
+for EXTENSION_NAME in $PLUGINS
+do
+  UPDATE=$(cat "plugin-config.json" | jq -r .Plugins[\"$EXTENSION_NAME\"][\"update\"])
+
+  if [ "$UPDATE" = "false" ]; then continue; fi
+
+  EXTENSION_REPOSITORY=$(cat "plugin-config.json" | jq -r .Plugins[\"$EXTENSION_NAME\"][\"repository\"])
+  EXTENSION_REVISION=$(cat "plugin-config.json" | jq -r .Plugins[\"$EXTENSION_NAME\"][\"revision\"])
+
+  CURRENT_REVISION=$(git ls-remote $EXTENSION_REPOSITORY HEAD)
+  CURRENT_REVISION=${CURRENT_REVISION:0:40}
+
+  if [[ $CURRENT_REVISION != $EXTENSION_REVISION ]]; then
+    STRING="[\"revision\"] |= \"$CURRENT_REVISION\""
+    FILE=$(cat "plugin-config.json" | jq -r ".Plugins[\"$EXTENSION_NAME\"][\"revision\"] |= \"$CURRENT_REVISION\"")
+    echo "${FILE}" > "plugin-config.json"
+
+    UPDATES=$UPDATES$EXTENSION_NAME$'\n'
+  fi
+
+done
+
+echo "$UPDATES"
