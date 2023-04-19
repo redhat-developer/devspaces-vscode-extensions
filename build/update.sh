3ff449a1cc14fea2c23fdcd216124c164cd5d071
sdawley <sdawley@redhat.com>
2023-04-19 10:41:14 -0700

    Update version update script and plugin versions. Set Jupyter to not auto-update

--- build/update.sh
+++ build/update.sh
@@ -23,6 +23,26 @@ do
   EXTENSION_REVISION=$(cat "plugin-config.json" | jq -r .Plugins[\"$EXTENSION_NAME\"][\"revision\"])
 
   CURRENT_REVISION=$(git ls-remote --tags --refs --sort='version:refname' $EXTENSION_REPOSITORY | tail --lines=1 | cut -d/ -f3)
+  #Special Cases
+  #ms-toolsai.jupyter is hard set for now (also versions are weird)
+  if [[ $EXTENSION_NAME = "dbaeumer.vscode-eslint" ]]; then
+    CURRENT_REVISION=$(git ls-remote --tags --refs $EXTENSION_REPOSITORY release* | tail --lines=1 | cut -d/ -f3,4)
+  fi
+  if [[ $EXTENSION_NAME = "llvm-vs-code-extensions.vscode-clangd" || $EXTENSION_NAME = "muhammad-sammy.csharp" ]]; then
+    CURRENT_REVISION=$(git ls-remote --tags --refs --sort='version:refname' $EXTENSION_REPOSITORY | grep -v 'v' | tail --lines=1 | cut -d/ -f3)
+  fi
+  if [[ $EXTENSION_NAME = "redhat.vscode-xml" ]]; then
+    CURRENT_REVISION=$(git ls-remote --tags --refs --sort='version:refname' $EXTENSION_REPOSITORY | grep -v -e 'v' -e 'latest' | tail --lines=1 | cut -d/ -f3)
+  fi
+  if [[ $EXTENSION_NAME = "redhat.fabric8-analytics" ]]; then
+    CURRENT_REVISION=$(git ls-remote --tags --refs --sort='version:refname' $EXTENSION_REPOSITORY | grep -v '2019' | tail --lines=1 | cut -d/ -f3)
+  fi
+  if [[ $EXTENSION_NAME = "ms-vscode.js-debug" ]]; then
+    CURRENT_REVISION=$(git ls-remote --tags --refs --sort='version:refname' $EXTENSION_REPOSITORY | grep -v '2020' | tail --lines=1 | cut -d/ -f3)
+  fi
+  if [[ $EXTENSION_NAME = "ms-python.python" ]]; then
+    CURRENT_REVISION=$(git ls-remote --tags --refs --sort='version:refname' $EXTENSION_REPOSITORY 20* | tail --lines=1 | cut -d/ -f3)
+  fi
   if [[ $CURRENT_REVISION = "" ]]; then
     CURRENT_REVISION=$(git ls-remote $EXTENSION_REPOSITORY HEAD)
     CURRENT_REVISION=${CURRENT_REVISION:0:40}
