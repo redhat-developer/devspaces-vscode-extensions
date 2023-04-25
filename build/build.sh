e75fd2b0aabf3708811ba4fdb1d5bcd8cab5ae9a
Samantha Dawley <sdawley@redhat.com>
2023-04-25 16:07:25 -0700

    Updates for 4049 (#60)
    * Add dockerfile for githistory. Update atlascode Dockerfile. Update build.sh
    
    * Fix atlascode Dockerfile. Make build.sh better (add clean option, argument checking... Add description to update.sh

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # Copyright (c) 2023 Red Hat, Inc.
 # This program and the accompanying materials are made
@@ -7,8 +7,30 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
 
-set -e
+CLEAN=0
+
+usage ()
+{
+    echo "Usage: $0 EXTENSION_NAME [--clean]
+
+Example: $0 atlassian.atlascode --clean
+
+Options: 
+  --clean     : Run podman system prune -a -f after the build to clean up leftover containers
+"
+    exit
+}
+
+if [[ -z "$1" ]]; then usage; fi
+if [[ ! -z "$2" ]] && [[ $2 = "--clean" ]]; then
+    CLEAN=1
+else
+    usage
+fi
 
 EXTENSION_NAME="$1"
 
@@ -46,7 +68,7 @@ fi
 
 echo "Building $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
 if test -f "$EXTENSION_NAME/Dockerfile"; then
-    docker build --no-cache=true \
+    podman build --no-cache=true \
         --build-arg extension_name="$EXTENSION_NAME" \
         --build-arg extension_repository="$EXTENSION_REPOSITORY" \
         --build-arg extension_revision="$EXTENSION_REVISION" \
@@ -55,7 +77,7 @@ if test -f "$EXTENSION_NAME/Dockerfile"; then
         --build-arg extension_vsce="$EXTENSION_VSCE" \
         -t "$EXTENSION_NAME"-builder "$EXTENSION_NAME"/
 else
-    docker build --no-cache=true \
+    podman build --no-cache=true \
         --build-arg extension_name="$EXTENSION_NAME" \
         --build-arg extension_repository="$EXTENSION_REPOSITORY" \
         --build-arg extension_revision="$EXTENSION_REVISION" \
@@ -66,12 +88,16 @@ else
 fi
 
 echo "Publishing $EXTENSION_NAME, version $EXTENSION_REPOSITORY"
-docker run --cidfile "$EXTENSION_NAME"-builder-id "$EXTENSION_NAME"-builder
+podman run --cidfile="$EXTENSION_NAME"-builder-id "$EXTENSION_NAME"-builder
 BUILDER_CONTAINER_ID=$(cat "$EXTENSION_NAME"-builder-id)
-docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME.vsix ./
-docker cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-sources.tar.gz ./
-docker stop $BUILDER_CONTAINER_ID
+podman cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME.vsix ./
+podman cp $BUILDER_CONTAINER_ID:/$EXTENSION_NAME-sources.tar.gz ./
+podman stop $BUILDER_CONTAINER_ID
 rm ./$EXTENSION_NAME-builder-id
+
+if [[ $CLEAN -eq 1 ]]; then
+    podman system prune -a -f
+fi
 
 # Get SHA256 of vsix and sources files and add to plugin-manifest.json for the Brew build
 PLUGIN_SHA=$(sha256sum $EXTENSION_NAME.vsix)
